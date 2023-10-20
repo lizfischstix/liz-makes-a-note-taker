d007cf9d2e9b4fea4eb5ef6ea41658f2f344193c
Liz FIschenich <Liz.Devlin85@gmail.com>
2023-10-20 17:23:43 -0500

    add readme file. attempt to create delete functionality, but thats a project for another day.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# liz-makes-a-note-taker
+# Liz Makes A Note Taker
+  ![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)
+  ## Description: 
+  This note taker allows a user to input notes and store notes,view the saved notes by title and bring the note up again by selecting the title.  It will help busy business owners...among others...stay organized. A delete function is currently in the works. 
+
+  ## Table of Contents: 
+  * [Installation](#installation)
+  * [License](#license)
+
+  ## Installation: 
+  this project can be accessed 
+
+  ## Contact:
+  @lizfischstix
+  ## License:
+    This project is licensed under the MIT license
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-//POST notes
+// const deleteNote = require('fs.unlink')
 
 //helper to gen unique note ID
 const uuid = require('../helper/uuid');
@@ -21,7 +21,8 @@ router.post('/notes', (req, res) => {
 console.log(req.body);
    //Need to read the db.json file and return all saved notes 
    console.info(`${req.method} input recieved for new notes`);
-   const { title, text } = req.body
+   const { title, text 
+   } = req.body
    if (title && text) {
       const newNote = {
          title,
@@ -33,6 +34,21 @@ console.log(req.body);
    res.json('working')
 });
 
-//need  post route that recieves new notes and saves 
+// //DELETE notes by uuid from api/notes
+// router.delete('/notes', (req, res) => {
+//    console.log(req.body);
+//       //Need to read the db.json file and delete selected note 
+//       console.info(`${req.method} note deleted`);
+//       const { title, text } = req.body
+//       if (title && text) {
+//          const deleteNote = {
+//             title,
+//             text,
+//             id: uuid(),
+//          }
+//          fs.unlink(deleteNote, './db/db.json');
+//       }
+//       res.json('working')
+//    });
 
 module.exports = router;
